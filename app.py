43972a1f1efe1d2206a3e451a04cd8afa955bf00
flo443 <florian.kehl19@gmail.com>
2021-10-12 14:02:19 +0200

    frontend init

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask.helpers import send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./frontend')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def default():
+    """
+    returns default html page
+    """
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9090, threaded=True)
