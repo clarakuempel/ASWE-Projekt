8527ad61d1beec6100db68d5550703bc3ca6d20a
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-10-14 23:11:42 +0200

    First functioning db connection

--- database.py
+++ database.py
@@ -1,16 +1,55 @@
-class DatabaseConnector:
-    __instance = None
+import abc
+from typing import Optional
+import secrets
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibmcloudant.cloudant_v1 import CloudantV1
 
-    @staticmethod
-    def get_instance():
+
+class DBInterface(metaclass=abc.ABCMeta):
+    # ToDo fully define interface
+    # @abc.abstractmethod
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Load in the file for extracting text."""
+        pass
+
+    # @abc.abstractmethod
+    def extract_text(self, full_file_name: str) -> dict:
+        """Extract text from the currently loaded file."""
+        raise Exception("Interface function is not implemented")
+
+
+class Database(DBInterface):
+    __instance = None
+    __service = None
+
+    @classmethod
+    def get_instance(cls) -> Optional[object]:
         """ Static access method. """
-        if DatabaseConnector.__instance is None:
-            DatabaseConnector()
-        return DatabaseConnector.__instance
+        if cls.__instance is None:
+            cls()
+        return cls.__instance
 
     def __init__(self):
         """ Virtually private constructor. """
-        if DatabaseConnector.__instance is not None:
+        if self.__class__.__instance is not None:
             raise Exception("This class is a singleton!")
         else:
-            DatabaseConnector.__instance = self
+            self.__class__.__instance = self
+
+    def connect(self) -> None:
+        authenticator = IAMAuthenticator(secrets.APIKey)
+        authenticator.set_disable_ssl_verification(True)
+        service = CloudantV1(authenticator=authenticator)
+        service.set_service_url(secrets.URL)
+        service.set_disable_ssl_verification(True)
+        self.__service = service
+
+    def get_service(self) -> CloudantV1:
+        return self.__service
+
+    def create_dbs(self):
+        if self.__service is None:
+            raise Exception("Call connect() first")
+        response = self.__service.put_database(db='products', partitioned=True).get_result()
+        if not response["ok"]:
+            raise Exception("Error while creation of databases")
