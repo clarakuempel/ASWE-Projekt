cb37f6364461fd624e9bbbf4d6a555a851f7c6c3
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-10-15 14:30:20 +0200

    Add pydoc, pre-commit

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 import abc
-from typing import Optional
 import secrets
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibmcloudant.cloudant_v1 import CloudantV1
@@ -23,20 +22,32 @@ class Database(DBInterface):
     __service = None
 
     @classmethod
-    def get_instance(cls) -> Optional[object]:
-        """ Static access method. """
+    def get_instance(cls) -> object:
+        """
+        Method to access singleton-instance. Creates instance if not exists.
+        :return: Singleton of database-connector
+        """
         if cls.__instance is None:
             cls()
         return cls.__instance
 
     def __init__(self):
-        """ Virtually private constructor. """
+        """
+        'Private' constructor.
+        Throws exception if singleton-instance already exists.
+        """
         if self.__class__.__instance is not None:
             raise Exception("This class is a singleton!")
         else:
             self.__class__.__instance = self
 
     def connect(self) -> None:
+        """
+        Connects to IBM Cloudant via IAMAuthenticator
+        based on API-Key and URL defined in ./secrets.py
+
+        :return: None
+        """
         authenticator = IAMAuthenticator(secrets.APIKey)
         authenticator.set_disable_ssl_verification(True)
         service = CloudantV1(authenticator=authenticator)
@@ -44,12 +55,16 @@ class Database(DBInterface):
         service.set_disable_ssl_verification(True)
         self.__service = service
 
+    def init(self):
+        pass
+
     def get_service(self) -> CloudantV1:
         return self.__service
 
-    def create_dbs(self):
+    def create_dbs(self) -> None:
         if self.__service is None:
             raise Exception("Call connect() first")
-        response = self.__service.put_database(db='products', partitioned=True).get_result()
+        response = self.__service\
+            .put_database(db='products', partitioned=True).get_result()
         if not response["ok"]:
             raise Exception("Error while creation of databases")
