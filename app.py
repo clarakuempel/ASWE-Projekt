1345c7815ebfe7812c780c8a7d171f73b95359ce
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2021-10-28 18:59:27 +0200

    Add watson test and debug chat html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import json
+import os
 import uuid
 
 from dotenv import load_dotenv
 from flask import Flask, request, session, jsonify
 from flask.helpers import send_from_directory
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import AssistantV2
 
 from database import Database
 from usecase import welcome
@@ -27,26 +29,51 @@ def default():
     return send_from_directory(app.static_folder, 'index.html')
 
 
-@app.route("/api/dialog")
+@app.route("/api/dialog", methods=['GET', 'POST'])
 def get_dialog_response():
-    user_input = request.form.get("input", "Hi")
-    print(f"Got user input: {user_input}")
-    # TODO Message Watson service and get response
-    res = json.loads("""{
-    "output": {
-        "intents": [
-            {
-                "intent": "General_Greetings",
-                "confidence": 0.9273388385772705
-            }
-        ]
-    }
-}""")
-    first_intent = res["output"]["intents"][0]["intent"]
-    if first_intent == "General_Greetings":
-        return jsonify(tts="I am Ivy. What's your name?")
-    else:
-        return jsonify(tts="I don't understand")
+    req_data = request.json
+    print(f"Got req data: {req_data}")
+
+    if not req_data:
+        return jsonify(tts="no input data")
+
+    user_input = req_data.get("input")
+
+    # if "watson" not in session:
+    #     session["watson"] = assistant.create_session(assistant_id=os.environ.get("WATSON_ASSISTANT_ID")).get_result()[
+    #         "session_id"]
+    #
+    # watson_res = assistant.message(
+    #     assistant_id=os.environ.get("WATSON_ASSISTANT_ID"),
+    #     session_id=session["watson"],
+    #     input={
+    #         'message_type': 'text',
+    #         'text': user_input
+    #     }
+    # ).get_result()
+
+    user_context = session.get("context", None)
+
+    watson_res = assistant.message_stateless(
+        assistant_id=os.environ.get("WATSON_ASSISTANT_ID"),
+        input={
+            'message_type': 'text',
+            'text': user_input
+        },
+        context=user_context
+    ).get_result()
+
+    session["context"] = watson_res["context"]
+    try:
+        return jsonify(tts=watson_res["output"]["generic"][0]["text"], watson=watson_res)
+    except Exception:
+        return jsonify(tts="no watson text defined", watson=watson_res)
+
+    # first_intent = watson_res["output"]["intents"][0]["intent"]
+    # if first_intent == "General_Greetings":
+    #    return jsonify(tts="I am Ivy. What's your name?", context=watson_res["context"], watson=watson_res)
+    # else:
+    #    return jsonify(tts="I don't understand", context=watson_res["context"], watson=watson_res)
 
 
 @app.route("/api/habits", methods=['GET'])
@@ -107,10 +134,22 @@ def test():
     return jsonify(output=text, preferences=prefs, session_id=session["id"])
 
 
+@app.route("/test_chat")
+def test_chat():
+    return send_from_directory(app.static_folder, 'watson_chat.html')
+
+
 if __name__ == "__main__":
     # Load .env file with secrets and credentials
     load_dotenv()
 
     Database.get_instance().initialize()
+
+    authenticator = IAMAuthenticator(os.environ.get("WATSON_ASSISTANT_API"))
+    authenticator.set_disable_ssl_verification(True)
+    assistant = AssistantV2(version='2021-06-14', authenticator=authenticator)
+
+    assistant.set_service_url('https://api.eu-de.assistant.watson.cloud.ibm.com')
+    assistant.set_disable_ssl_verification(True)
 
     app.run(debug=True, host='0.0.0.0', port=9090, threaded=True)
