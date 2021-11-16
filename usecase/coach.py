c81d9bc9514fb9d9438a6e00aa03c5287c40aea5
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2021-11-16 19:25:00 +0100

    Add load_data() to sports coach

--- usecase/coach.py
+++ usecase/coach.py
@@ -5,3 +5,51 @@ Use Case 3
 - gym $gym.name, $gym.auslastung
 - video $video.title
 """
+
+import random
+
+from service import api, utility
+
+
+def load_data():
+    rapla_lectures, rapla_current_lecture, rapla_next_lecture = None, None, None
+    rapla_data = api.get_rapla().json()
+    events = utility.get_events(rapla_data)
+    if "rapla_lectures" in events.keys():
+        rapla_lectures = events["rapla_lectures"]
+    if "rapla_current_lecture" in events.keys():
+        rapla_current_lecture = events["rapla_current_lecture"]
+    if "rapla_next_lecture" in events.keys():
+        rapla_next_lecture = events["rapla_next_lecture"]
+
+    yt_data = api.get_youtube_search("home workout pamela reif 20 min").json()
+    yt = utility.parse_youtube_search(yt_data)
+    r = random.randrange(0, 10)
+    video = {
+        "title": yt[r]["title"],
+    }
+
+    gym_data = api.get_gym_utilization(1731421430).json()
+    utilization = utility.parse_gym_utilization(gym_data)
+    gym = {
+        "auslastung": utilization,
+        "name": "McFIT Stuttgart-Mitte"
+    }
+
+    weather_data = api.get_weather_forecast(48.783333, 9.183333).json()
+    weather, icon = utility.get_current_weather(weather_data)
+
+    return {
+        "rapla_lectures": rapla_lectures,
+        "rapla_current_lecture": rapla_current_lecture,
+        "rapla_next_lecture": rapla_next_lecture,
+        "video": video,
+        "gym": gym,
+        "weather": {
+            "min": weather["min"],
+            "max": weather["max"],
+            "current": weather["current"],
+            "rain": weather["rain"],
+            "description": weather["description"]
+        }
+    }
