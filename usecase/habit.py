57f79c2d2d9715827531421764990e6bd043d2a7
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2021-11-16 18:36:00 +0100

    Add load_data() to habit tracker

--- usecase/habit.py
+++ usecase/habit.py
@@ -4,3 +4,24 @@ Use Case 2
 - Book $book
 - Bed time $now?, $bed_time
 """
+import random
+
+from service import api, utility
+
+
+def load_data():
+    quote_data = api.get_quote()
+    quote, _ = utility.parse_quote(quote_data)
+
+    book_data = api.get_bestselling_books().json()
+    books = utility.parse_bestselling_books(book_data)
+    r = random.randrange(0, 10)
+    book = f"'{books[r]['title'].title()}' by {books[r]['author']}"
+
+    # Todo -> Load database preferences and get "wake up" time to calc $bed_time
+
+    return {
+        "quote": quote,
+        "book": book,
+        "bed_time": "24 Uhr"
+    }
